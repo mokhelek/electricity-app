2927b7d297f9b2e6fdb392ccdde81b60d0501032
mokhelek <mkatleho191@gmail.com>
2023-05-30 13:57:25 +0200

    added passing tests

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,65 @@
 // DOM element(s) references
+// const topUpRadio = document.querySelector(".topup:checked"); // *my top-up options
+const unitsAvailableElem = document.querySelector(".unitsAvailable") ;
+const totalUnitsBoughtElem = document.querySelector(".totalUnits") ;
+const totalAmountSpentElem = document.querySelector(".totalAmount") ;
+const advanceTakenElem = document.querySelector(".advanceTaken") ;
 
 
+const topupNowBtn = document.querySelector(".topupNow");
+const useNowBtn = document.querySelector(".useNow");
 
 // Factory Function instance 
 const electricity =  Electricity();
 
 
+function displayInfo(){
+    unitsAvailableElem.innerHTML = electricity.getUnitsAvailable() ;
+    totalUnitsBoughtElem.innerHTML = electricity.totalUnitsBought() ;
+    totalAmountSpentElem.innerHTML = electricity.totalAmountSpent() ;
+
+    // IF THERE IS ADVANCED TAKEN THEN I SHOULD SHOW THE ADVANCED GREEN TICK
+
+    if(electricity.advanceTaken()){
+        advanceTakenElem.classList.remove("hidden")
+    }
+}
+
+
+function topupBtnClicked(){
+    const topUpRadio = document.querySelector(".topup:checked"); // * my top-up options
+    if(topUpRadio.value){
+        if(topUpRadio.value != "advance"){
+            electricity.topUpElectricity(Number(topUpRadio.value)) ;
+        }else{
+            electricity.topUpElectricity(topUpRadio.value) ;
+        }
+ 
+    }else{
+        alert("top up option blank")
+    }
+
+    displayInfo()
+}
+
+
+function useNowBtnClicked(){
+    const usageRadio = document.querySelector(".usage:checked"); // * my use appliances options
+    if(usageRadio.value){
+        electricity.useAppliance(usageRadio.value) ;
+    }else{
+        alert("select usage option")
+    }
+
+    displayInfo()
+}
+
+
 // DOM events here 
+topupNowBtn.addEventListener("click", function(){
+    topupBtnClicked()
+})
+
+useNowBtn.addEventListener("click",function(){
+    useNowBtnClicked()
+})
--- electricity.js
+++ electricity.js
@@ -1,37 +1,96 @@
 function Electricity() {
-
-    // do we want to go with this or array? 
+    // do we want to go with this or array?
     let appliances = {
-        'Stove': 10, 
-        'Kettle': 5, 
-        'TV': 3, 
-        'Fridge': 13
+        Stove: 10,
+        Kettle: 5,
+        TV: 3,
+        Fridge: 13,
     };
 
+    // * Electricity units prices
+    let unitsPrice = {
+        10: 7,
+        20: 14,
+        50: 35,
+        advance: 21,
+    };
+
+    let unitsAvailable = 0; // total units available
+
+    let advancedAmount = 0; // * This should be money, not units
+
+    let theTotalAmountSpent = 0;
+    let theTotalUnitsBought = 0;
+
     function topUpElectricity(amount) {
+        /*
+         * if the amount is advance
+         * check if the user owes any money
+         * if they owe any money then they cannot take any more advance
+         * if the amount is a top-up
+         * then pay the advance money
+         * meaning that the amount that I buy the units with should be difference after the debt money is payed
+         */
 
+        // if you want advanced but you did not fully pay
+        if (amount == "advance" && advancedAmount > 0) {
+            return "pay full amount";
+        } else {
+            if (advanceTaken()) {
+                // * if the debt is more than what the user recharges then take all the money and no units added
+                if (advancedAmount >= amount) {
+                    advancedAmount -= amount;
+                } else {
+                    let remainingAfterDeduction = amount - advancedAmount;
+                    // * debt is cleared
+                    advancedAmount = 0;
+                    unitsAvailable += unitsPrice[remainingAfterDeduction];
+                }
+            } else {
+                if (amount == "advance") {
+                    advancedAmount = 30;
+                }
+                unitsAvailable += unitsPrice[amount];
+            }
+        }
 
+        if (amount != "advance") {
+            theTotalAmountSpent += amount;
+            theTotalUnitsBought += unitsPrice[amount];
+        }
     }
 
     function getUnitsAvailable() {
-         return unitsAvailable;
+        return unitsAvailable;
     }
 
     /*
-    * return true and substract from unit available if there is enough units to use the appliance
-    * other wise return false and do nothing.
-    */
+     * return true and subtract from unit available if there is enough units to use the appliance
+     * other wise return false and do nothing.
+     */
     function useAppliance(appliance) {
-        
+        if (unitsAvailable >= appliances[appliance]) {
+            unitsAvailable -= appliances[appliance];
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function advanceTaken() {
+        if (advancedAmount > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     function totalAmountSpent() {
+        return theTotalAmountSpent;
     }
 
-    function totalUnitsBought(){
+    function totalUnitsBought() {
+        return theTotalUnitsBought;
     }
 
     return {
@@ -40,7 +99,6 @@ function Electricity() {
         getUnitsAvailable,
         useAppliance,
         totalAmountSpent,
-        totalUnitsBought
-
-    }
+        totalUnitsBought,
+    };
 }
